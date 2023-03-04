e843b105b34855fc289954c2ea9e7896fb634493
Urey Mutuale <urey@octobytes.com>
2023-03-04 14:54:05 +0100

    policies updated

--- src/Policies/PermissionAbilityPolicy.php
+++ src/Policies/PermissionAbilityPolicy.php
@@ -2,29 +2,51 @@
 
 namespace BigFiveEdition\Permission\Policies;
 
+use Exception;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class PermissionAbilityPolicy
 {
 	use HandlesAuthorization;
 
-	public function hasAbility($model, $ability, $resource = null): bool
+	protected $abilities;
+
+	public function before($user, $abilities)
 	{
-		$type = $resource ? get_class($resource) : null;
-		$id = $resource ? Arr::get($resource, 'id') : null;
+		$this->abilities = $abilities;
+	}
 
-		if (stripos($ability, '|')) {
-			$abilities = is_array($ability) ? $ability : explode('|', $ability);
-			return $model->hasAnyAbilitiesOn($abilities, $type, $id);
-		} else if (stripos($ability, '&')) {
-			$abilities = is_array($ability) ? $ability : explode('&', $ability);
-			return $model->hasAllAbilitiesOn($abilities, $type, $id);
-		} else {
-			$abilities = is_array($ability) ? $ability : [$ability];
-			return $model->hasAnyAbilitiesOn($abilities, $type, $id);
+	/**
+	 * Determine whether the model has the passed ability on the resource
+	 *
+	 * @param  $user
+	 * @param  $ability
+	 * @param  $resource
+	 * @return Response|bool
+	 */
+	public function hasAbility($user, $ability, $resource = null): Response|bool
+	{
+		try {
+			$type = $resource ? get_class($resource) : null;
+			$id = $resource ? Arr::get($resource, 'id') : null;
+
+			if (stripos($ability, '|')) {
+				$abilities = is_array($ability) ? $ability : explode('|', $ability);
+				return $user->hasAnyAbilitiesOn($abilities, $type, $id);
+			} else if (stripos($ability, '&')) {
+				$abilities = is_array($ability) ? $ability : explode('&', $ability);
+				return $user->hasAllAbilitiesOn($abilities, $type, $id);
+			} else {
+				$abilities = is_array($ability) ? $ability : [$ability];
+				return $user->hasAnyAbilitiesOn($abilities, $type, $id);
+			}
+		} catch (Exception $e) {
+			Log::error($e->getMessage());
+			Log::error($e->getTraceAsString());
 		}
-
 		return false;
 	}
 }
