62258deb8ae1db7fa39a1871eb7aead61237e044
Urey Mutuale <ureymutuale@hotmail.fr>
2023-03-09 18:38:39 +0100

    policies updated

--- src/Policies/PermissionRolePolicy.php
+++ src/Policies/PermissionRolePolicy.php
@@ -2,23 +2,45 @@
 
 namespace BigFiveEdition\Permission\Policies;
 
+use Exception;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Log;
 
 class PermissionRolePolicy
 {
 	use HandlesAuthorization;
 
-	public function hasRole($model, $role): bool
+	protected $abilities;
+
+	public function before($user, $abilities)
 	{
-		if (stripos($role, '|')) {
-			$roles = is_array($role) ? $role : explode('|', $role);
-			return $model->hasAnyRoles($roles);
-		} else if (stripos($role, '&')) {
-			$roles = is_array($role) ? $role : explode('&', $role);
-			return $model->hasAllRoles($roles);
-		} else {
-			$roles = is_array($role) ? $role : [$role];
-			return $model->hasAnyRoles($roles);
+		$this->abilities = $abilities;
+	}
+
+	/**
+	 * Determine whether the model has the passed role
+	 *
+	 * @param $user
+	 * @param $role
+	 * @return Response|bool
+	 */
+	public function hasRole($user, $role): Response|bool
+	{
+		try {
+			if (stripos($role, '|')) {
+				$roles = is_array($role) ? $role : explode('|', $role);
+				return $user->hasAnyRoles($roles);
+			} else if (stripos($role, '&')) {
+				$roles = is_array($role) ? $role : explode('&', $role);
+				return $user->hasAllRoles($roles);
+			} else {
+				$roles = is_array($role) ? $role : [$role];
+				return $user->hasAnyRoles($roles);
+			}
+		} catch (Exception $e) {
+			Log::error($e->getMessage());
+			Log::error($e->getTraceAsString());
 		}
 		return false;
 	}
--- src/Policies/PermissionTeamPolicy.php
+++ src/Policies/PermissionTeamPolicy.php
@@ -2,23 +2,45 @@
 
 namespace BigFiveEdition\Permission\Policies;
 
+use Exception;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Log;
 
 class PermissionTeamPolicy
 {
 	use HandlesAuthorization;
 
-	public function belongsToTeam($model, $team): bool
+	protected $abilities;
+
+	public function before($user, $abilities)
 	{
-		if (stripos($team, '|')) {
-			$teams = is_array($team) ? $team : explode('|', $team);
-			return $model->belongsToAnyTeams($teams);
-		} else if (stripos($team, '&')) {
-			$teams = is_array($team) ? $team : explode('&', $team);
-			return $model->belongsToAllTeams($teams);
-		} else {
-			$teams = is_array($team) ? $team : [$team];
-			return $model->belongsToAnyTeams($teams);
+		$this->abilities = $abilities;
+	}
+
+	/**
+	 * Determine whether the model belongs to the passed team
+	 *
+	 * @param $user
+	 * @param $role
+	 * @return Response|bool
+	 */
+	public function belongsToTeam($user, $team): Response|bool
+	{
+		try {
+			if (stripos($team, '|')) {
+				$teams = is_array($team) ? $team : explode('|', $team);
+				return $user->belongsToAnyTeams($teams);
+			} else if (stripos($team, '&')) {
+				$teams = is_array($team) ? $team : explode('&', $team);
+				return $user->belongsToAllTeams($teams);
+			} else {
+				$teams = is_array($team) ? $team : [$team];
+				return $user->belongsToAnyTeams($teams);
+			}
+		} catch (Exception $e) {
+			Log::error($e->getMessage());
+			Log::error($e->getTraceAsString());
 		}
 		return false;
 	}
