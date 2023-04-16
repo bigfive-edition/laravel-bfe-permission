0302e5dbffb71429d577e8f9e6c405e901e11b13
Urey Mutuale <ureymutuale@hotmail.fr>
2023-04-16 17:02:24 +0100

    repos. + abilities operations

--- config/bfe-permission.php
+++ config/bfe-permission.php
@@ -4,7 +4,7 @@ return [
 
 	'default_teams' => [],
 	'default_roles' => ['sysadmin', 'admin', 'pro_user', 'regular_user'],
-	'ability_operations' => ['read_all', 'read_one', 'create_one', 'update_one', 'delete_one'],
+	'ability_operations' => ['read_all', 'read_one', 'read_owned', 'create_one', 'update_one', 'update_owned', 'delete_one', 'delete_owned'],
 	'ability_resources' => [
 		\BigFiveEdition\Permission\Models\Team::class,
 		\BigFiveEdition\Permission\Models\TeamModel::class,
