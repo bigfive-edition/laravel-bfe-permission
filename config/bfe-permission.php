80dd9dcdf82b995e250d885bd308702453ec9d72
Urey Mutuale <ureymutuale@hotmail.fr>
2023-04-16 17:14:03 +0100

    default config updated

--- config/bfe-permission.php
+++ config/bfe-permission.php
@@ -3,8 +3,8 @@
 return [
 
 	'default_teams' => [],
-	'default_roles' => ['sysadmin', 'admin', 'pro_user', 'regular_user'],
-	'ability_operations' => ['read_all', 'read_one', 'read_owned', 'create_one', 'update_one', 'update_owned', 'delete_one', 'delete_owned'],
+	'default_roles' => ['sysadmin', 'application_admin', 'community_manager', 'client'],
+	'ability_operations' => ['read_all', 'read_all_owned', 'read', 'read_owned', 'create', 'update', 'update_owned', 'delete', 'delete_owned'],
 	'ability_resources' => [
 		\BigFiveEdition\Permission\Models\Team::class,
 		\BigFiveEdition\Permission\Models\TeamModel::class,
--- src/Contracts/AbilityOperationType.php
+++ src/Contracts/AbilityOperationType.php
@@ -5,13 +5,14 @@ namespace BigFiveEdition\Permission\Contracts;
 abstract class AbilityOperationType
 {
 	const READ_ALL = 'read_all';
-	const READ_OWNED = 'read_owned';
+	const READ_ALL_OWNED = 'read_all_owned';
 	const READ = 'read';
+	const READ_OWNED = 'read_owned';
 	const CREATE = 'create';
 	const UPDATE = 'update';
 	const UPDATE_OWNED = 'update_owned';
 	const DELETE = 'delete';
 	const DELETE_OWNED = 'delete_owned';
 
-	const ALL = ['read_all', 'read', 'create', 'update', 'delete'];
+	const ALL = ['read_all', 'read_all_owned', 'read', 'read_owned', 'create', 'update', 'update_owned', 'delete', 'delete_owned'];
 }
